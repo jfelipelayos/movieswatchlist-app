800967103c5e22df353cada1bd339936917f9059
Felipe Layos <jfelipelayos@gmail.com>
2022-01-19 13:56:10 -0500

    Watchlist component

---
+++ src/components/Watchlist/Watchlist.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
+import MovieCard from '../Cards/MovieCard/MovieCard'
+import './Watchlist.css'
+import '../MainView/MainView.css'
+
+
+function Watchlist() {
+    const { watchlist } = useContext(GlobalContext)
+
+    return (
+        <div className='main-container'>
+            <div className="main-container-info">
+                <h3>{watchlist.length}</h3>
+                <h2 className='main-container-title' data-testid="mainview-watch-title">Peliculas por ver</h2>
+            </div>
+            {
+                watchlist.length > 0 ? (
+                    <div className="main-container-movies">
+                        {
+                            watchlist.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie} type="watchlist" />
+                            ))
+                        }
+                    </div>
+                ) : (
+                    <h3>Lista vacia...</h3>
+                )
+            }
+
+        </div>
+    )
+}
+
+export default Watchlist
